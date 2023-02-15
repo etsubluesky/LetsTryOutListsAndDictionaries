19bf66f7bda796d308d33ce533d7a958382176b7
Desjardins, Mathew Ryan <DESJARDINSM@etsu.edu>
2023-02-14 20:36:32 -0500

    Add project files.

---
+++ LetsTryOutListsAndDictionaries/Program.cs
@@ -0,0 +1,65 @@
+﻿using LetsTryOutListsAndDictionaries;
+//using System.Linq;
+
+List<string> names = new List<string>();
+names.Add("Matt");
+names.Add("Matt");
+names.Add("Matt");
+names.Add("Matt");
+names.Add("Matt");
+
+List<int> numbers = new List<int>(100);
+
+Console.WriteLine(names.Capacity);
+Console.WriteLine(numbers.Capacity);
+
+
+
+List<State> states = new List<State>(50);
+
+var state = new State();
+state.Id = 2;
+state.Name = "Connecticut";
+state.Abbreviation = "CT";
+state.URLToFlagPicture = " https://www.nationsonline.org/gallery/USA/CoA/Connecticut-Flag-226x136.jpg";
+
+states.Add(state);
+
+state = new State();
+state.Id = 3;
+state.Name = "Georgia";
+state.Abbreviation = "GA";
+state.URLToFlagPicture = " https://www.nationsonline.org/gallery/USA/CoA/Georgia-Flag-226x141.jpg";
+
+states.Add(state);
+
+states.Add(new State { Id = 1, Abbreviation = "TN", Name = "Tennessee", URLToFlagPicture = "https://www.nationsonline.org/gallery/USA/CoA/Tennessee-Flag-226x136.jpg" });
+
+//var refState = states[2];
+
+//Console.WriteLine(states.Count);
+
+//Console.WriteLine(states.Contains( refState));
+
+//refState = states.Find(s => s.Name == "Tennessee");
+foreach (var item in states)
+{
+	Console.WriteLine(item.Name);
+}
+
+var chunks = states.Chunk(1);
+
+Console.WriteLine(chunks.Count());
+
+foreach (var items in chunks)
+{
+	foreach (var item in items)
+	{
+		Console.WriteLine(item.Name);
+	}
+
+	//states.
+	//	Console.WriteLine("End chunk");
+}
+//Console.WriteLine(states.Contains(refState));
+//Console.WriteLine(states.Contains(states.Where(s => s.Name == "Tennessee").First()));
---
+++ LetsTryOutListsAndDictionaries/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsTryOutListsAndDictionaries
+{
+    public class State
+    {
+        //primary key in the database this is stored
+        public int Id { get; set; }
+        public string Abbreviation { get; set; }
+        public string Name { get; set; }
+        public string URLToFlagPicture { get; set; }
+       
+
+
+    }
+}
